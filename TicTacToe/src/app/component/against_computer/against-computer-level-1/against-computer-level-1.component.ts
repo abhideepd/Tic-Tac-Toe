65e11b7f4fcc1b8bbfc46e2927bd4d0f0e476359
Abhideep Das <abhideepd@gmail.com>
2025-07-18 10:16:27 +0530

    build for deployment on git pages

--- TicTacToe/src/app/component/against_computer/against-computer-level-1/against-computer-level-1.component.ts
+++ TicTacToe/src/app/component/against_computer/against-computer-level-1/against-computer-level-1.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 })
 export class AgainstComputerLevel1Component implements OnInit{
   board: string[][]=[];
+  boardWin: boolean[][]=[];
   currentPlayer: string='X';
   winner: string|null=null;
   moves:number=0;
@@ -41,6 +42,7 @@ export class AgainstComputerLevel1Component implements OnInit{
 
   resetBoard(){
     this.board=Array(this.boardSize).fill(null).map(()=>Array(this.boardSize).fill(''));
+    this.boardWin=Array(this.boardSize).fill(false).map(()=>Array(this.boardSize).fill(false));
     this.currentPlayer='X';
     this.winner=null;
     this.moves=0;
@@ -93,15 +95,23 @@ export class AgainstComputerLevel1Component implements OnInit{
   checkWinner(row:number, col:number):boolean{
     const player=this.currentPlayer;
     if(this.board[row].every(cell=>cell===player)){
+      this.boardWin[row].map(r=>r=true);
+      console.log(this.boardWin);
       return true;
     }
     if(this.board.every(r=>r[col]===player)){
+      this.boardWin.map(r=>r[col]=true);
+      console.log(this.boardWin);
       return true;
     }
     if(row===col && this.board.every((r,i)=>r[i]===player)){
+      this.boardWin.map((r,i)=>r[i]=true);
+      console.log(this.boardWin);
       return true;
     }
     if(row+col === (this.boardSize-1) && this.board.every((r,i)=>r[(this.boardSize-1)-i]===player)){
+      this.boardWin.map((r,i)=>r[(this.boardSize-1)-i]=true);
+      console.log(this.boardWin);
       return true;
     }
     return false;
